101cf9b6be736433e3a8b7eaa5c782319e018453
Nick Kallen <nickkallen@me.com>
2020-03-19 12:33:38 +0100

    buttons show proper text

--- python2.7libs/hviz.py
+++ python2.7libs/hviz.py
@@ -2,6 +2,7 @@ import hou, traceback, sys
 from hou import parmTemplateType
 from PySide2 import QtCore, QtWidgets, QtGui
 from PySide2.QtCore import Qt
+import math
 
 this = sys.modules[__name__]
 
@@ -29,9 +30,26 @@ this.viz = None
 
 DPI=2 # FIXME
 
-class Foo(QtWidgets.QWidget):
+class Overlay(QtWidgets.QWidget):
+    @staticmethod
+    def summarize(parm_tuple):
+        epsilon = 0.0001
+        vs = []
+        for v in parm_tuple.eval():
+            type = parm_tuple.parmTemplate().type()
+            if type == parmTemplateType.Float:
+                if v - math.floor(v) < epsilon:
+                    vs.append("{:.0f}".format(v))
+                else:
+                    vs.append("{:.1f}".format(v))
+            else:
+                vs.append(str(v))
+        v = vs[0] if len(vs) == 1 else ",".join(vs)
+        kvp = "{}={}".format(parm_tuple.name(), v)
+        return kvp[0:20]
+
     def __init__(self, editor, parent=None):
-        super(Foo, self).__init__(parent)
+        super(Overlay, self).__init__(parent)
         self._editor = editor
         self._setup_ui()
 
@@ -47,19 +65,37 @@ class Foo(QtWidgets.QWidget):
         self.setStyleSheet("QWidget{background-color:rgba(1,1,1,0.1)}")
         self.grabKeyboard()
 
-        for (item, rect) in self._editor.allVisibleRects(()):
+
+        for item, rect in self._editor.allVisibleRects(()):
             if not isinstance(item, hou.Node): continue
-            button = QtWidgets.QPushButton(text=item.name(), parent=self)
+
             posx = self._editor.posToScreen(rect.max()).x()/DPI
             posy = size.y()/DPI - self._editor.posToScreen(rect.min()).y()/DPI
             unit = self._editor.lengthToScreen(1)/DPI
             marginx = unit/10
-            marginy = unit/22
-            button.move(posx + marginx, posy + marginy)
-            import math
-            font_size = math.ceil(self._editor.lengthToScreen(1)/DPI/6)
-            button.setStyleSheet("QPushButton{padding:0;font-size: " + str(font_size) + "px; background-color: rgb(38,56,76);}");
-            button.show()
+            marginy = -unit/30
+            posx += marginx
+            posy += marginy
+           
+            initialposx = posx
+            initialposy = posy
+            for i, parm_tuple in enumerate(item.parmTuples()):
+                if parm_tuple.isHidden(): continue
+                if parm_tuple.isAtDefault(): continue
+                type = parm_tuple.parmTemplate().type()
+                if not type in (parmTemplateType.Int, parmTemplateType.Float, parmTemplateType.String, parmTemplateType.Toggle): continue
+
+                button = QtWidgets.QPushButton(text=Overlay.summarize(parm_tuple), parent=self)
+                button.move(posx, posy)
+                font_size = math.ceil(self._editor.lengthToScreen(1)/DPI/6)
+                button.setStyleSheet("QPushButton{padding:0;font-size: " + str(font_size) + "px; background-color: rgb(38,56,76);}");
+                button.show()
+                posx += button.frameGeometry().width()
+                if posx-initialposx > unit:
+                    posx = initialposx
+                    posy += button.frameGeometry().height()
+                if posy-initialposy >= 0.5*unit:
+                    break
 
         self.setAutoFillBackground(False)
 
@@ -71,7 +107,7 @@ class Foo(QtWidgets.QWidget):
             this.viz = None
             return True
 
-        return super(Foo, self).event(event)
+        return super(Overlay, self).event(event)
     
     def paintEvent(self, event):
         opt = QtWidgets.QStyleOption()
@@ -80,7 +116,7 @@ class Foo(QtWidgets.QWidget):
         self.style().drawPrimitive(QtWidgets.QStyle.PE_Widget, opt, painter, self)
 
 def visualize(uievent):
-    viz = Foo(uievent.editor, hou.qt.mainWindow())
+    viz = Overlay(uievent.editor, hou.qt.mainWindow())
 
 
     viz.show()
